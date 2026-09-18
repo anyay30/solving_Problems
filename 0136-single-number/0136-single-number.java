ee7b629dd7825633c8365de5d97d2001ceaff8bb
Anya <riya.2005yadav@gmail.com>
2026-09-18 19:30:29 +0530

    Time: 14 ms (14.44%), Space: 46.7 MB (91.85%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int c=0;
-        for(int i:nums)
-        c^=i;
-        return c;
+      HashMap<Integer, Integer> hs=new HashMap<>();
+      for(int i=0;i<nums.length;i++){
+        hs.put(nums[i],hs.getOrDefault(nums[i],0)+1);
+      }  
+      //  Find the number that appears only once
+        for (int key : hs.keySet()) {
+            if (hs.get(key) == 1) {
+                return key;
+            }
+        }
+        return -1;
     }
 }
